2cb3c845e2f913b6becc256001979fad11c0e117
hema18deena@gmail.com <hema18deena@gmail.com>
2024-08-16 15:55:46 +0000

    updates

--- code/preprocess.py
+++ code/preprocess.py
@@ -9,6 +9,6 @@ def preprocess(df):
 if __name__ == "__main__":
     base_dir = "/opt/ml/processing"
 
-    df = pd.read_csv(f"{base_dir}/input/syntehetic_data.csv", header=None)
+    df = pd.read_csv(f"{base_dir}/input/synthetic_data.csv", header=None)
     processed_df = preprocess(df)
     processed_df.to_csv(f"{base_dir}/output/preprocessed_data.csv", index=False)
