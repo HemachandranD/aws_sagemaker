56c4d8410d888952d3d5ed69e51f14b5d5645b4b
hema18deena@gmail.com <hema18deena@gmail.com>
2024-08-17 07:21:50 +0000

    updates

--- code/train.py
+++ code/train.py
@@ -1,5 +1,6 @@
 from prophet import Prophet
 import pandas as pd
+import os
 
 
 
@@ -12,6 +13,6 @@ def train():
 if __name__ == "__main__":
     # The input data directory is typically stored in /opt/ml/input/data/<channel_name>
     train_input_path = os.environ['SM_CHANNEL_TRAIN']
-    
+
     df = pd.read_csv(f"{train_input_path}/train.csv")
     train()
